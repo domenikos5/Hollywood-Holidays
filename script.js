c09e755afb093f180adbf50f71a865c283239950
Dominador G de la Cruz Jr <domenikos858@gmail.com>
2020-04-11 10:53:29 -0700

    initial

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(() => {
+    $('#searchForm').on('submit'. (e) => {
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
+    });
+});
+
+function getMovies(searchText){
+    axios.get('http://www.omdbapi.com?s='+searchText)
+    .then((response) => {
+      console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      $.each(movies, (index, movie) => {
+          output += `   
+          <div class="col-md-3">
+            <div class="well text-center">
+            <img src="${movie.Poster}">
+            <h5>${movieTitle}</h5>
+            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+            </div>       
+          `;
+      });
+
+      $('#movies').html(output);
+
+
+    })
+    .catch((err) => {
+        console.log(err);
+
+    });
+}
+
+function movieSelected(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
